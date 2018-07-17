0962e9e74ea884e901ea62f92ca523ed603f9d38
GautamManoharan <gautam.raw@gmail.com>
2018-07-18 09:34:33 +1000

    zoom keys added

--- linisher.py
+++ linisher.py
@@ -58,18 +58,25 @@ def Rect(y,angle):
     glPopMatrix()
 
 def get_y(angle):
-    latan = atan((1+1)/0.2)*180/3.14
-    watan = atan((1+0.2)/1)*180/3.14
+    lw_radius = 1;#Linish Wheel radius
+    length = 2
+    lb2=length/2
+    width = 0.4
+    wb2=width/2
+    sqrt_l2pw2 = 1.0198
+    vertex_radius = 1.01;#length/2,width/2 < vertex_radius < sqrt((l/2)^2+(w/2)^2)
+    latan = atan((lw_radius+lb2)/wb2)*180/3.14
+    watan = atan((lw_radius+wb2)/lb2)*180/3.14
     if (angle > 0 and angle < (90 - latan)):
-        y = (1+1) / sin((angle+90)*3.14/180)
+        y = (lw_radius+lb2) / sin((angle+90)*3.14/180)
     elif (angle > watan and angle < (180 - watan)):
-        y = (1+0.2) / sin(angle * 3.14 / 180)
+        y = (lw_radius+wb2) / sin(angle * 3.14 / 180)
     elif (angle > (90+latan) and angle < (270 - latan)):
-        y = (1+1) / sin((angle-90) * 3.14 / 180)
+        y = (lw_radius+lb2) / sin((angle-90) * 3.14 / 180)
     elif (angle > (180+watan) and angle < (360-watan)):
-        y = (1+0.2) / sin((angle-180) * 3.14 / 180)
+        y = (lw_radius+wb2) / sin((angle-180) * 3.14 / 180)
     elif (angle > (270+latan) and angle <= 360):
-        y = (1+1) / sin((angle-270)*3.14/180)
+        y = (lw_radius+lb2) / sin((angle-270)*3.14/180)
     else:
         y = 0
     return 1-y
@@ -91,12 +98,20 @@ def main():
 
     gluPerspective(45, (display[0]/display[1]), 0.1, 50.0)
 
-    glTranslatef(0.0,0.0, -5)
+    prev_pressed=False
+    #zoom_position = -5
+    glTranslatef(0.0,0.0, -5.0)
 
     glMatrixMode(GL_MODELVIEW)
 
-    latan = atan((1+1)/0.2)*180/3.14
-    watan = atan((1+0.2)/1)*180/3.14
+    lw_radius = 1;#Linish Wheel radius
+    length = 2
+    lb2=length/2
+    width = 0.4
+    wb2=width/2
+    latan = atan((lw_radius+lb2)/wb2)*180/3.14
+    watan = atan((lw_radius+wb2)/lb2)*180/3.14
+
     a=0
     while True:
         a=a+1
@@ -107,6 +122,17 @@ def main():
             if event.type == pygame.QUIT:
                 pygame.quit()
                 quit()
+            if (event.type == pygame.KEYDOWN) and (prev_pressed==False):
+                if event.key == pygame.K_DOWN:
+                    #zoom_position = zoom_position - 0.5
+                    glTranslatef(0.0,0.0, -0.5)
+                if event.key == pygame.K_UP:
+                    #zoom_position = zoom_position + 0.5
+                    glTranslatef(0.0,0.0, 0.5)
+                prev_pressed=True
+            else:
+                prev_pressed=False
+        #glTranslatef(0.0,0.0, zoom_position)
 
         glClear(GL_COLOR_BUFFER_BIT|GL_DEPTH_BUFFER_BIT)
         cCircle(a)
